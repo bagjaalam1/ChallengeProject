40cec88bc87787e373da15550ce610c61246e5af
bagjaalam1 <bagjaalam1@gmail.com>
2024-03-04 17:00:45 +0700

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChallengeProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
