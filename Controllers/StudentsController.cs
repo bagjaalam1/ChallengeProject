f38074291d26480d639a45a72237f82298dd85a5
bagjaalam1 <bagjaalam1@gmail.com>
2024-03-05 21:25:23 +0700

    Create and Read

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,54 @@
+﻿using ChallengeProject.Data;
+using ChallengeProject.Models;
+using ChallengeProject.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChallengeProject.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public StudentsController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddStudentViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var student = new Student(viewModel.Name, viewModel.Email, viewModel.Phone);
+
+                try
+                {
+                    await dbContext.Students.AddAsync(student);
+                    await dbContext.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "An error occurred saving the student.");
+                }
+            }
+
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var students = await dbContext.Students.ToListAsync();
+
+            return View(students);
+        }
+    }
+}
